35fff305343cdb57f4d502764e99ba2175496f20
Dimas Aditia Saputra <dimasaditiasaputra122@gmail.com>
2025-04-07 14:49:49 +0700

    setup migration database

---
+++ database/migrations/2025_04_07_065957_create_todolist_table.php
@@ -0,0 +1,30 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('todolist', function (Blueprint $table) {
+            $table->id();
+            $table->string('task', 255);
+            $table->boolean('is_completed')->default(false);
+            $table->timestamp('due_date')->nullable();
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('todolist');
+    }
+};
